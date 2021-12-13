2143fbc8d4d785310a0f37133ac6d8b1780146db
White Void <51837452+LeiteQualhado@users.noreply.github.com>
2021-12-13 16:11:02 -0300

    Organizando Projeto

---
+++ src/components/MainBody/MainBody.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MainBody extends Component {
+    render() {
+        return (
+            <main>
+                
+            </main>
+        )
+    }
+}
+
+export default MainBody;
